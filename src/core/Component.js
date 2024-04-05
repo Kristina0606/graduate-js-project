c1add140691e10715bd955b556a8c3821b10fa70
kristina <krisbubenka@icloud.com>
2024-04-05 23:23:09 +0300

    add core element

---
+++ src/core/Component.js
@@ -0,0 +1,41 @@
+import Handlebars from "handlebars";
+
+export class Component extends HTMLElement {
+  constructor() {
+    super();
+    this.state = {};
+    this.props = {};
+    this.template = null; ///// для работы с компайлингом
+  }
+
+  setState(callback) {
+    ///// обновляем состояние объекта
+    this.state = callback(this.state);
+    this.compile();
+  }
+
+  compile() {
+    /// использует текущий шаблон, компилирует его с помощью Handlebars.compile
+    // и обновляет содержимое элемента с помощью полученного HTML.
+    const template = Handlebars.compile(this.template);
+    this.innerHTML = "";
+    this.innerHTML = template(this.state);
+  }
+
+  connectedCallback() {
+    // Вызывается, когда компонент добавляется в DOM.
+    this.compile();
+    this.componentDidMount();
+  }
+
+  disconnectedCallback() {
+    // Вызывается, когда компонент удаляется из DOM.
+    this.componentWillUnmount;
+  }
+
+  componentDidMount() {} //  Этот метод может быть переопределен в дочерних классах
+  // для выполнения дополнительных действий после добавления компонента в DOM.
+
+  componentWillUnmount() {} //  Этот метод может быть переопределен в дочерних классах
+  // для выполнения дополнительных действий после удаления компонента из DOM.
+}
