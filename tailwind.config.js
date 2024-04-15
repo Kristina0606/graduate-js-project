05fd2070aac82fdddf1e377552fe2d0fcca99a55
kristina <krisbubenka@icloud.com>
2024-04-15 23:39:53 +0300

    add ui home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,hbs}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        layout: "max-content 1fr max-content",
+      },
+      backgroundImage: {
+        workspace:
+          'url("https://images.unsplash.com/photo-1512149074996-e923ac45be6a?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+      },
+    },
   },
   plugins: [require("tailwindcss-animated")],
 };
