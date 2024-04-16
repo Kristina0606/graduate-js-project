240538c094dbb44dbac46fcd63b8f0431c83e59b
kristina <krisbubenka@icloud.com>
2024-04-16 21:39:28 +0300

    added ui sign in and sing up pages

--- src/pages/sign-in/sign-in.component.js
+++ src/pages/sign-in/sign-in.component.js
@@ -1,7 +1,9 @@
 import { Component } from "../../core/Component";
 import template from "./sign-in.template.hbs";
-
 import { ROUTES } from "../../constants/routes";
+
+import "../../components/input/input.component";
+import "../../components/button/button.component";
 
 export class SignIn extends Component {
   constructor() {
--- src/pages/sign-up/sign-up.component.js
+++ src/pages/sign-up/sign-up.component.js
@@ -1,7 +1,10 @@
 import { Component } from "../../core/Component";
 import template from "./sign-up.template.hbs";
-
 import { ROUTES } from "../../constants/routes";
+
+import "../../components/input/input.component";
+import "../../components/button/button.component";
+import "../../components/router-link/router-link.component";
 
 export class SignUp extends Component {
   constructor() {
