08bc8b62aa0c4dc9785d760b98d7b1ec567fec61
kristina <krisbubenka@icloud.com>
2024-04-17 13:11:53 +0300

    add loader with error

--- src/pages/sign-in/sign-in.component.js
+++ src/pages/sign-in/sign-in.component.js
@@ -4,14 +4,21 @@ import { ROUTES } from "../../constants/routes";
 
 import "../../components/input/input.component";
 import "../../components/button/button.component";
+import "../../components/loader/loader.component";
 
 export class SignIn extends Component {
   constructor() {
     super();
+    this.state = {
+      errors: {
+        email: "",
+      },
+      isLoading: false,
+    };
+
     this.template = template({
       routes: ROUTES,
     });
-    this.state = {};
   }
 }
 
